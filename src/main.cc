befb8e1b06cabe2656f01bb169c9c671625a9d20
Alies Krepelka <28122632+AliesKrepelka@users.noreply.github.com>
2026-04-10 18:43:19 -0500

    Adds GPU Handling(No more using integrated graphics)
    Export variables to enable high performance for NVIDIA and AMD graphics.

--- src/main.cc
+++ src/main.cc
@@ -267,6 +267,12 @@ MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 	}
 }
 
+extern "C" {
+    __declspec(dllexport) unsigned long NvOptimusEnablement = 1;
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
+
 int main(int argc, char* argv[])
 {
 	std::string window_title = "Menger";
